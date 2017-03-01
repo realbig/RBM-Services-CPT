7118c11de34063a7a08fe07fcbb8a3f05a2256d0
Eric Defore <eric.defore@gmail.com>
2017-03-01 08:46:59 -0500

    v0.1.1

--- rbm-services-cpt.php
+++ rbm-services-cpt.php
@@ -3,7 +3,7 @@
 Plugin Name: RBM Services CPT
 Plugin URL: https://github.com/realbig/RBM-Services-CPT
 Description: Services CPT moved from CPT-onomies
-Version: 0.1.0
+Version: 0.1.1
 Text Domain: rbm-services-cpt
 Author: Eric Defore
 Author URL: http://realbigmarketing.com
